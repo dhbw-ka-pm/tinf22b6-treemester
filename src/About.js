6d02d520686f659f9292b809b8ae155bd4c97911
FurkanK <122597204+FurKay00@users.noreply.github.com>
2023-07-02 15:12:51 +0200

    feat: navigator items

--- src/About.js
+++ src/About.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './App.css';
 
 function About() {
     return (
         <>
             <div className="description">
-                <h3>Circle Packing Model</h3>
-                <p>Circle Packing is a modified version of a treemap where circles are used instead of rectangles.
-                    
-                    The circles symbolize different levels in the hierarchy with each branch of the tree represented by a circle and its sub-branches shown as circles contained within it.
-                    
-                    Additionally, the size of each circle can be utilized to convey an extra value, such as quantity or file size. Furthermore, color can be employed to assign categories or represent another variable by using different shades.
-                 </p>
+                <h2>Treemester</h2>
+                <p class = "blockSatz">
+                Treemester is a software for students and other people,
+                who are struggling with learning and are in need of better topical guidance.
+                We differ from other software by creating a product that establishes a fun and easy
+                learning experience with an appealing and professional design at the same time! 
+                </p>
             </div>
         </>
     );
--- src/Navigation.js
+++ src/Navigation.js
@@ -16,14 +16,61 @@ function Navigation() {
 
     return (
         <nav className="navigation">
-            <div className="links">
+            <div className="start">
                 <Link to="/tinf22b6-treemester" onClick={() => setCurrent("circlePacking")}>
                     <p className="label">&#x1F333; Treemester</p>
                 </Link>
-                    <Link to="/tinf22b6-treemester/about" onClick={() => setCurrent("about")} className={classNameAbout}>
-                        <span>About the Project</span>
-                    </Link>
+
             </div>
+            <div className="links">
+
+                <Link to="/tinf22b6-treemester/about" onClick={() => setCurrent("about")} className={classNameAbout}>
+                    <span>About Treemester</span>
+                </Link>
+
+                <Link to="/tinf22b6-treemester" onClick={() => setCurrent("circlePacking")}>                    
+                    <span>Login</span>
+                </Link>
+                    
+                <Link to="/tinf22b6-treemester" onClick={() => setCurrent("circlePacking")}>
+                    <span>Edit</span>
+                    <ul>
+                        <li>
+                            Create new Mindmap 
+                        </li>
+                        
+                        <li>
+                            Upload Mindmap
+                        </li>
+
+                        <li>
+                            Create new Sprintline
+                        </li>
+                        
+                        <li>
+                            Upload Sprintline 
+                       </li>
+                   </ul>
+                </Link>
+                
+                <Link to="/tinf22b6-treemester" onClick={() => setCurrent("circlePacking")} >
+                    <span>Settings</span>
+                        <ul>
+                            <li>
+                                Options
+                            </li>
+                            <li>
+                                Darkmode
+                            </li>
+                            <li>
+                                Help
+                            </li>
+                            <li>
+                                "Logged User"
+                            </li>
+                        </ul>
+                    </Link>            
+                </div>
         </nav>
     )
 }
