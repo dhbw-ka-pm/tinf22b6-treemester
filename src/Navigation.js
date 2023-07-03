389c4628d740158cfcdaabd906a44a1290f6d6bb
FurkanK <122597204+FurKay00@users.noreply.github.com>
2023-07-03 19:26:51 +0200

    Revert "fix: list items are distinguished by background color"
    This reverts commit 56e4eb711dcf9d7b7c6e8dd6a55d5bf4fb8d31fa.

--- src/Navigation.js
+++ src/Navigation.js
@@ -38,12 +38,15 @@ function Navigation() {
                         <li>
                             Create new Mindmap 
                         </li>
+                        
                         <li>
                             Upload Mindmap
                         </li>
+
                         <li>
                             Create new Sprintline
                         </li>
+                        
                         <li>
                             Upload Sprintline 
                        </li>
