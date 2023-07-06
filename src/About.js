e03c65f960ca8c1e66396a10b0038fcb90b185f7
FurkanK <122597204+FurKay00@users.noreply.github.com>
2023-07-07 00:03:28 +0200

    feat: FAQ prototype

--- src/About.js
+++ src/About.js
@@ -4,14 +4,26 @@ import './App.css';
 function About() {
     return (
         <>
-            <div className="description">
-                <h2>Treemester</h2>
-                <p className = "blockSatz">
-                Treemester is a software for students and other people,
-                who are struggling with learning and are in need of better topical guidance.
-                We differ from other software by creating a product that establishes a fun and easy
-                learning experience with an appealing and professional design at the same time! 
-                </p>
+            <div className = "faq">
+                <h1>Treemester</h1>
+                <hr></hr>
+                <h3>What is Treemester about? </h3>
+                    Treemester is a software for students and other people,
+                    who are struggling with learning and are in need of better topical guidance.
+                    We differ from other software by creating a product that establishes a fun and easy
+                    learning experience with an appealing and professional design at the same time! 
+                <hr></hr>
+                <h3>How to create your first Mindmap</h3>
+                    tba
+                <hr></hr>
+                <h3>What is a SprintLine?</h3> 
+                    A Sprintline is your personal roadmap! Choose a mindmap, select the topics you want to 
+                    learn and create your own learning journey. You can designate the required To-Do's for 
+                    each subtopic and thus, check your current learning state!
+                <hr></hr>
+                <h3>Can I save my Mindmaps and SprintLines?</h3>
+                    Yes, you can save both of them and you are also able to upload them for further editing!
+                <hr></hr>
             </div>
         </>
     );
